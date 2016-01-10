c4bea673fec2392684d7a7c3bb77063f27218277
SmartyMoon <smarty@lizengweideiMac.local>
2016-01-10 20:33:52 +0800

    change

--- app/index.php
+++ app/index.php
@@ -16,7 +16,6 @@
 if (version_compare(PHP_VERSION, '5.3.0', '<')) {
     die('require PHP > 5.3.0 !');
 }
-echo 'bbq';
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG', true);
@@ -26,5 +25,6 @@ define('APP_PATH', './Application/');
 
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
+
 
 // 亲^_^ 后面不需要任何代码了 就是如此简单
